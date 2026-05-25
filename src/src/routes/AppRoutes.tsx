2734d918cf3c9033a3b314e3a84062fafe022988
Freymar Sanchez <freymar.sanchez@wiedii.co>
2026-05-25 00:10:41 -0500

    config vite and base path project

--- src/src/routes/AppRoutes.tsx
+++ src/src/routes/AppRoutes.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { MainLayout } from '../components/layout/MainLayout'
-import { HomePage } from '../pages/Home/HomePage'
-import { MemberDetailPage } from '../pages/Members/MemberDetailPage'
-import { MembersPage } from '../pages/Members/MembersPage'
-import { StoriesPage } from '../pages/Stories/StoriesPage'
-import { WorkDetailPage } from '../pages/Works/WorkDetailPage'
-import { WorksPage } from '../pages/Works/WorksPage'
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { MainLayout } from "../components/layout/MainLayout";
+import { HomePage } from "../pages/Home/HomePage";
+import { MemberDetailPage } from "../pages/Members/MemberDetailPage";
+import { MembersPage } from "../pages/Members/MembersPage";
+import { StoriesPage } from "../pages/Stories/StoriesPage";
+import { WorkDetailPage } from "../pages/Works/WorkDetailPage";
+import { WorksPage } from "../pages/Works/WorksPage";
 
 export function AppRoutes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<MainLayout />}>
           <Route index element={<HomePage />} />
@@ -20,6 +20,6 @@ export function AppRoutes() {
           <Route path="cuentos" element={<StoriesPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
-  )
+    </HashRouter>
+  );
 }
